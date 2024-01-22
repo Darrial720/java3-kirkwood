0c20f11e60b16c0d7438e0b7ea66113971a5b4d6
Darrial729 <dlsjr0102@aol.com>
2024-01-22 13:09:58 -0600

    updating java 3

--- src/main/java/edu/kirkwood/demo/ArtistFromJson.java
+++ src/main/java/edu/kirkwood/demo/ArtistFromJson.java
@@ -1,4 +1,14 @@
 package edu.kirkwood.demo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 public class ArtistFromJson {
+    @JsonProperty("data")
+    private List<Artist> artists;
+
+    public List<Artist> getArtists() {
+        return artists;
+    }
 }
