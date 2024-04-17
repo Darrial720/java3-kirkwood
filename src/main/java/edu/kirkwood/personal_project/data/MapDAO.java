1682468cbd98ad4f94df192c5c6fa1312303c2d1
Darrial729 <dlsjr0102@aol.com>
2024-04-17 12:38:41 -0500

    Day 23 quiz

--- src/main/java/edu/kirkwood/personal_project/data/MapDAO.java
+++ src/main/java/edu/kirkwood/personal_project/data/MapDAO.java
@@ -2,6 +2,9 @@ package edu.kirkwood.personal_project.data;
 
 
 import edu.kirkwood.personal_project.model.Map;
+import edu.kirkwood.personal_project.model.UserTwo;
+import edu.kirkwood.shared.CommunicationService;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
@@ -35,4 +38,44 @@ public class MapDAO extends Database{
         }
         return maps;
     }
+
+    public static void deleteMap(Map map){
+        try (Connection connection = getConnection()) {
+            if (connection != null) {
+                try (CallableStatement statement = connection.prepareCall("{CALL sp_delete_map(?)}")) {
+                    statement.setInt(1, map.getMapID());
+                    statement.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String createMap(Map map) {
+        try (Connection connection = getConnection();
+             CallableStatement statement = connection.prepareCall("{CALL sp_create_map(?, ?, ?, ?)}")
+        ) {
+            statement.setString(1, user.getEmail());
+            String hashedPassword = BCrypt.hashpw(String.valueOf(user.getPassword()), BCrypt.gensalt(12));
+            statement.setString(2, hashedPassword);
+            int rowsAffected = statement.executeUpdate();
+            if (rowsAffected == 1) {
+                try (CallableStatement statement2 = connection.prepareCall("{CALL sp_get_2fa_code(?)}");) {
+                    statement2.setString(1, user.getEmail());
+                    ResultSet resultSet = statement2.executeQuery();
+                    if (resultSet.next()) {
+                        String code = resultSet.getString("code");
+                        String method = resultSet.getString("method");
+                        if (method.equals("email")) {
+                            return CommunicationService.sendNewUserEmail2(user.getEmail(), code);
+                        }
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Likely error with stored procedure");
+            System.out.println(e.getMessage());
+        }
+        return "";
 }
